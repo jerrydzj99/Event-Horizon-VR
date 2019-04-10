c9ec5d692579e770af1546cec468c2e6073332d3
Jerry Ding <jerryding2022@berkeley.edu>
2019-04-09 22:42:40 -0700

    added pod to main scene, prototype pod door open script

---
+++ Event Horizon VR/Assets/Scripts/PodOpen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodOpen : MonoBehaviour
+{
+    int count = 0;
+    Vector3 rotation;
+    Vector3 pivot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Quaternion temp = this.gameObject.GetComponentInParent<Transform>().rotation;
+        rotation = new Vector3(temp.x, temp.y, temp.z);
+        pivot = new Vector3(-.6287f, 2.5052f, 8.903f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count < 750)
+        {
+            this.gameObject.GetComponent<Transform>().RotateAround(pivot, rotation, -.1f);
+        }
+        count++;
+    }
+}
