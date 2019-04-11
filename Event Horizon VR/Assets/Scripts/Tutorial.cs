44c97deb864f1f81a79bf4d5e159e21699d2cb21
Eric Li <eric.li5009@berkeley.edu>
2019-04-10 18:50:55 -0700

    added tutorial script and animation

---
+++ Event Horizon VR/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public float duration;
+    public GameObject player;
+
+    Animator anim;
+    AudioSource beeping;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        beeping = GetComponent<AudioSource>();
+        beeping.Play();
+        StartCoroutine(tutorial());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator tutorial()
+    {
+        yield return new WaitForSeconds(duration);
+        beeping.Stop();
+        anim.SetTrigger("Open");
+        yield return new WaitForSeconds(3f);
+        player.transform.Find("Ambience").gameObject.SetActive(true);
+    }
+}
