947d41535e6d19792b07ce9e59af8d8c7602933b
Jerry Ding <jerryding2022@berkeley.edu>
2019-04-11 02:03:03 -0700

    fix bugs

---
+++ Event Horizon VR/Assets/Scripts/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered this method");
+        if (other.gameObject.CompareTag("Player")) {
+            animator.SetBool("character_nearby", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("character_nearby", false);
+        }
+    }
+}
